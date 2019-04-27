ae5b7be233ba6769332a1bf9020debfc699cfe20
Daniel <torreswilches@gmail.com>
2019-04-27 10:42:08 -0500

    Agregamos Variables tipos datos y conversiones

--- index.py
+++ index.py
@@ -5,5 +5,27 @@ Editor de Spyder
 Este es un archivo temporal.
 """
 
+# comentario de linea
+
+"""
+comentario de parrafo
+"""
+
 a = 5
 A = 7
+c = 4
+
+print("Hola Mundo")
+
+# variables
+
+x = 4
+y = "Texto"
+z = 4.5
+a1 = True
+b = False
+c = 4+5j
+
+
+print(x, y, z, a, b, c)
+print(type (b))
