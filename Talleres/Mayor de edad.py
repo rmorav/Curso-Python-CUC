5f199098734bdd698b5d6c22d4126bb02a9b3ae4
Rafael Mora <rexmor2_2@hotmail.com>
2019-05-04 09:35:41 -0500

    Cargue en segunda sesion

---
+++ Talleres/Mayor de edad.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 09:08:01 2019
+
+@author: android
+"""
+print("Digite su edad")
+edad = input()
+edad = int(edad)
+"""if edad >= 18:
+    print("ud es mayor de edad")
+else:
+    print("ud es menor de edad")    
+"""
+
+# si de linea con sino
+print("ud es mayor de edad") if edad >= 18 else print("ud es menor de edad") 
